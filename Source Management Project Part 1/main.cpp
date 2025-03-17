2818206053b72970bb1caed54455e4125d5129e6
Alexis Robinson <alexis.robinson3617@sinclair.edu>
2025-03-17 16:25:51 -0400

    Initial Commit

---
+++ Source Management Project Part 1/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Source Management Project Part 1
+//
+//  Created by Alexis Robinson on 3/17/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
